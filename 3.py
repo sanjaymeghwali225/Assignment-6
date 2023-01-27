f39118a2229e456e4229156028458088a7753f57
sanjay meghwal <121242177+sanjaymeghwali225@users.noreply.github.com>
2023-01-27 23:11:37 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,9 @@
+from math import factorial
+
+n=5
+for i in range(n):
+    for j in range(n-1-i):
+        print(" ", end="")
+    for k in range (i+1):
+        print(factorial(i) // (factorial(i-k)*factorial(k)) , end=" ") # nCr = n! / ((n-r)! * r!)
+    print()
---
+++ 4.py
@@ -0,0 +1,13 @@
+def checkpanagram(str):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for char in alphabet:
+        if char in str.lower():
+            return True
+        else:
+            return False
+        
+input_string=input("Enter chosen string here ")
+if checkpanagram(input_string.lower())==True:
+    print(input_string," is a panagram")
+else:
+    print(input_string, " is not a panagram ")
